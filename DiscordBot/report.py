3e6f553c411d7471abcf221f46e90872c58c6bcd
Alex Yansouni <40583979+AlexJYRed@users.noreply.github.com>
2023-05-24 09:24:57 -0700

    Update report.py

--- DiscordBot/report.py
+++ DiscordBot/report.py
@@ -8,6 +8,20 @@ class State(Enum):
     MESSAGE_IDENTIFIED = auto()
     REPORT_COMPLETE = auto()
 
+class Reason(Enum):
+    SPAM = auto()
+    HARASSMENT = auto()
+    DOXING = auto()
+    REPORT_OTHER = auto()
+    OTHER = auto()
+    UNKNOWN = auto()
+
+class DoxingSubReason(Enum):
+    SENSITIVE_INFO = auto()
+    THREATENING_LEAK = auto()
+    EXPOSING_INFO = auto()
+    UNKNOWN = auto()
+
 class Report:
     START_KEYWORD = "report"
     CANCEL_KEYWORD = "cancel"
@@ -17,14 +31,11 @@ class Report:
         self.state = State.REPORT_START
         self.client = client
         self.message = None
-    
-    async def handle_message(self, message):
-        '''
-        This function makes up the meat of the user-side reporting flow. It defines how we transition between states and what 
-        prompts to offer at each of those states. You're welcome to change anything you want; this skeleton is just here to
-        get you started and give you a model for working with Discord. 
-        '''
+        self.reason = Reason.UNKNOWN
+        self.doxing_subreason = DoxingSubReason.UNKNOWN
+        self.imminent_danger = None
 
+    async def handle_message(self, message):
         if message.content == self.CANCEL_KEYWORD:
             self.state = State.REPORT_COMPLETE
             return ["Report cancelled."]
@@ -38,7 +49,6 @@ class Report:
             return [reply]
         
         if self.state == State.AWAITING_MESSAGE:
-            # Parse out the three ID strings from the message link
             m = re.search('/(\d+)/(\d+)/(\d+)', message.content)
             if not m:
                 return ["I'm sorry, I couldn't read that link. Please try again or say `cancel` to cancel."]
@@ -52,21 +62,55 @@ class Report:
                 message = await channel.fetch_message(int(m.group(3)))
             except discord.errors.NotFound:
                 return ["It seems this message was deleted or never existed. Please try again or say `cancel` to cancel."]
-
-            # Here we've found the message - it's up to you to decide what to do next!
             self.state = State.MESSAGE_IDENTIFIED
             return ["I found this message:", "```" + message.author.name + ": " + message.content + "```", \
-                    "This is all I know how to do right now - it's up to you to build out the rest of my reporting flow!"]
+                    "What is your reason for reporting? You can say `spam`, `harassment`, `doxing`, `reporting on behalf of someone else`, or `other`."]
         
         if self.state == State.MESSAGE_IDENTIFIED:
-            return ["<insert rest of reporting flow here>"]
+            if self.reason == Reason.UNKNOWN:
+                return ["Please specify the reason for your report."]
+            elif self.reason == Reason.SPAM or self.reason == Reason.HARASSMENT or self.reason == Reason.REPORT_OTHER or self.reason == Reason.OTHER:
+                self.state = State.REPORT_COMPLETE
+                return ["Thank you for the report. Our moderation team will take appropriate action. Would you like to block the user? You can say `yes` or `no`."]
+            elif self.reason == Reason.DOXING:
+                if self.doxing_subreason == DoxingSubReason.UNKNOWN:
+                    return ["Please specify. You can say `sensitive information about me`, `threatening to leak my information`, or `exposing my information`."]
+                elif self.doxing_subreason != DoxingSubReason.UNKNOWN:
+                    if self.imminent_danger == None:
+                        return ["Does the information present an imminent physical danger to you? You can say `yes` or `no`."]
+                    else:
+                        self.state = State.REPORT_COMPLETE
+                        if self.imminent_danger:
+                            return ["Your report has been received. Our moderation team will investigate the situation and resolve the situation as soon as possible. In the meantime, would you like to block the user? You can say `yes` or `no`."]
+                        else:
+                            return ["Thank you for the report. Our moderation team will take appropriate action. Would you like to block the user? You can say `yes` or `no`."]
 
-        return []
+        if self.state == State.MESSAGE_IDENTIFIED and self.reason == Reason.UNKNOWN:
+            if message.content.lower() == "spam":
+                self.reason = Reason.SPAM
+            elif message.content.lower() == "harassment":
+                self.reason = Reason.HARASSMENT
+            elif message.content.lower() == "doxing":
+                self.reason = Reason.DOXING
+            elif message.content.lower() == "reporting on behalf of someone else":
+                self.reason = Reason.REPORT_OTHER
+            elif message.content.lower() == "other":
+                self.reason = Reason.OTHER
+
+        if self.state == State.MESSAGE_IDENTIFIED and self.reason == Reason.DOXING and self.doxing_subreason == DoxingSubReason.UNKNOWN:
+            if message.content.lower() == "sensitive information about me":
+                self.doxing_subreason = DoxingSubReason.SENSITIVE_INFO
+                return ["We will remind the user of our doxing policy. Your report has been received. Our moderation team will take appropriate action. Would you like to block the user? You can say `yes` or `no`."]
+            elif message.content.lower() == "threatening to leak my information":
+                self.doxing_subreason = DoxingSubReason.THREATENING_LEAK
+            elif message.content.lower() == "exposing my information":
+                self.doxing_subreason = DoxingSubReason.EXPOSING_INFO
+
+        if self.state == State.MESSAGE_IDENTIFIED and self.reason == Reason.DOXING and self.doxing_subreason != DoxingSubReason.UNKNOWN and self.imminent_danger == None:
+            if message.content.lower() == "yes":
+                self.imminent_danger = True
+            elif message.content.lower() == "no":
+                self.imminent_danger = False
 
     def report_complete(self):
         return self.state == State.REPORT_COMPLETE
-    
-
-
-    
-
